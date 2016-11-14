1d28650f06a63080fb654e6ad4d0a0333babd0cc
Gregor <gregor@Gregors-MacBook-Pro.local>
2016-11-14 12:02:25 +0100

    gregor neuester stand

---
+++ www/js/services.js
@@ -0,0 +1,29 @@
+angular.module('taskQL')
+	.factory('LoginService', function($q) {
+    return {
+        loginUser: function(name, pw) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+ 			 var jsondata = {
+					username : name,
+					password : pw
+					};
+			 var loggingIn = $http.post('https://alpha.taskql.com/rest/api/1/taskql/login',jsondata);
+			 loggingIn.success(function(data,status,headers,config) {
+		  		console.log(data);	  
+                deferred.resolve('Welcome ' + name + '!');
+            } else {
+                deferred.reject('Wrong credentials.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+})
