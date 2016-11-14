a4108ec47f6cdba7e9ad5842a9be544f15cdbbaf
Katharina Usbek <katharina.usbek@web.de>
2016-11-14 21:42:17 +0100

    login input angepasst

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('taskQL').controller('loginController', function($scope) {
     $scope.credentials= {};
  
     $scope.login = function() {
-        LoginService.loginUser($scope.credentials.username, $scope.credentials.password).success(function(credentials) {
+        LoginService.loginUser($scope.credentials.email, $scope.credentials.password).success(function(credentials) {
             $state.go('tab.dash');
         }).error(function(data) {
             var alertPopup = $ionicPopup.alert({
