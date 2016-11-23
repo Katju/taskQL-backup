55665cdf7a886397f3506bcbe7e9ff2f912c3e04
Yahima <norealnumber@gmail.com>
2016-11-23 15:31:05 +0100

    dashboard Projects

--- www/js/services.js
+++ www/js/services.js
@@ -5,7 +5,6 @@ angular
     	    getLogin: function(usernameInput, passwordInput) {
     	    	var url = 'https://alpha.taskql.com/rest/api/1/taskql/login';
     			var loginData = JSON.stringify({username: usernameInput, password: passwordInput});
-<<<<<<< HEAD
     			
     			return $http.post(url, loginData, { cache: true});
     	    },
@@ -23,19 +22,3 @@ angular
     	}
 	 })
 	
-=======
-  
-    			return $http.post(url, loginData);
-    	    }
-    	};
-    })
-    .factory('RestGet', function($http) {
-    		return {
-    			getRestGet: function(restGetUrl) {
-    				var url='https://alpha.taskql.com/rest/api/1/project/getAll';
-					return $http.get(restGetUrl);
-				}
-	 };
-	 });
-    		;
->>>>>>> origin/master
