72cd5858502b48810d773ef313874c89007ac9a2
Katharina Usbek <katharina.usbek@web.de>
2016-11-26 22:08:44 +0100

    controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,28 +6,28 @@ angular
     })
 
     .controller('loginController', function(Login, $scope, $rootScope, $location) {
-    	$scope.login = function(){ 
-    		Login.getLogin($scope.username, $scope.password).then(function(response){
-    			$rootScope.loginResponse = response.data;
-    			$rootScope.sessionToken = response.data.SessionToken;
-    		    $location.path('dashboard');
-    		    Login.getAll($rootScope.sessionToken).then(function(response){
-    		        $rootScope.getAllResponse = response.data;
+        $scope.login = function(){ 
+            Login.getLogin($scope.username, $scope.password).then(function(response){
+                $rootScope.loginResponse = response.data;
+                $rootScope.sessionToken = response.data.SessionToken;
+                $location.path('dashboard');
+                Login.getAll($rootScope.sessionToken).then(function(response){
+                    $rootScope.getAllResponse = response.data;
 
-    	        }).catch(function(response){
-    	            //request was not successful
-    	            //handle the error
-    	        });	
-    		    
-	        }).catch(function(response){
-	            //request was not successful
-	            //handle the error
-	        });
-    	}	
+                }).catch(function(response){
+                    //request was not successful
+                    //handle the error
+                }); 
+                
+            }).catch(function(response){
+                //request was not successful
+                //handle the error
+            });
+        }   
     })
      
     .controller('dashboardController', function($scope) {
-    	
+        
     })
 
     .controller('projecteditorController', function($scope){
