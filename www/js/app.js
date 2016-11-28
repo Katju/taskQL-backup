6f5c19152a969f73bcae92898ed831043c32dff2
Katharina Usbek <katharina.usbek@web.de>
2016-11-28 12:09:40 +0100

    app

--- www/js/app.js
+++ www/js/app.js
@@ -9,14 +9,14 @@ angular.module('taskQL', ['ionic'])
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory
-		// bar above the keyboard
+    // bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // Don't remove this line unless you know what you are doing. It stops
-		// the viewport
+    // the viewport
       // from snapping when text inputs are focused. Ionic handles this
-		// internally for
+    // internally for
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
