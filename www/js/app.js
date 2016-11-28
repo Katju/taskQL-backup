69458025b50984779cce76f22f949c417d0971c0
Katharina Usbek <katharina.usbek@web.de>
2016-11-28 16:24:07 +0100

    dashboard subprojects state

--- www/js/app.js
+++ www/js/app.js
@@ -46,6 +46,11 @@ angular.module('taskQL', ['ionic'])
       templateUrl: 'templates/dashboard.html'
    })
 
+    .state('dashboard_subproject',  {
+      url:'/dashboard_subproject',
+      templateUrl: 'templates/dashboard_subproject.html'
+    })
+
     .state('projecteditor', {
       url:'/projecteditor',
       templateUrl: 'templates/projecteditor.html',
