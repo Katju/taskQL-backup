7b5fe72f086c6f780cf2d10e807185167927e5bc
Yahima <norealnumber@gmail.com>
2016-12-07 15:53:42 +0100

    delete Project + some code improvements

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,103 +1,149 @@
 angular
-    .module('taskQL')
-    .controller('mainController', function(mainFactory, $scope, $rootScope, $location, $timeout, $ionicPopup) {
-    	
-	 	$scope.login = function(){ 
-        
-            mainFactory.getLogin($scope.username, $scope.password).then(function(response){
-                
-                $rootScope.loginResponse = response.data;
-                $rootScope.sessionToken = response.data.SessionToken;
-                $location.path('dashboard');
-                
-               mainFactory.getAll($rootScope.sessionToken).then(function(response){
-                	
-                	$rootScope.getAllResponse = response.data;
-
-                }).catch(function(response){
-                    //request was not successful
-                    //handle the error
-                });
-                
-            }).catch(function(response){
-                //request was not successful
-                //handle the error
-            });
-        }
-        
-        $scope.getSubprojects = function(projectID){
-            
-            mainFactory.getProjectInfo(projectID, $rootScope.sessionToken).then(function(response){
-                
-                $rootScope.getProjectInfoResponse = response.data;
-                $location.path('dashboard_subproject');
-
-            }).catch(function(response){
-                //request was not successful
-                //handle the error
-            }); 
-        }
-        
-        $scope.renameProject = function(projectID, projectTitle){
-
-        	var inputPopup = $ionicPopup.prompt({
-    		   
-        		title: 'Rename Project',
-        		template: 'Enter new name',
-        		inputType: 'text',
-        		inputPlaceholder: projectTitle
-    		   
-        	   	}).then(function(res) {
-        		   
-        	   		if(res) {
+	.module('taskQL')
+	.controller('mainController', function(mainFactory, $scope, $rootScope, $location, $timeout, $ionicPopup) {
 	
-        	   			mainFactory.rename(projectID, res, $rootScope.sessionToken);
-        	   			mainFactory.getAll($rootScope.sessionToken).then(function(response){
+		$scope.login = function(){ 
+	
+			mainFactory.getLoginReq($scope.username, $scope.password).then(function(response){
+	
+				$rootScope.loginResponse = response.data;
+				$rootScope.sessionToken = response.data.SessionToken;
+				$location.path('dashboard');
+	
+				mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+	
+					$rootScope.getAllRes = response.data;
+	
+				}).catch(function(response){
+					// request was not successful
+					// handle the error
+				});
+	
+			}).catch(function(response){
+				// request was not successful
+				// handle the error
+			});
+		}
+	
+		$scope.getSubprojects = function(projectID){
+	
+			mainFactory.getProjectInfoReq(projectID, $rootScope.sessionToken).then(function(response){
+	
+				$rootScope.getProjectInfoRes = response.data;
+				$location.path('dashboard_subproject');
+	
+			}).catch(function(response){
+				// request was not successful
+				// handle the error
+			}); 
+		}
+	
+		$scope.renameProject = function(projectID, projectTitle){
+	
+			var renameInput = $ionicPopup.prompt({
+	
+				title: 'Rename Project',
+				template: 'Enter new name',
+				inputType: 'text',
+				inputPlaceholder: projectTitle
+	
+			}).then(function(res) {
+	
+				if(res) {
+	
+					mainFactory.renameProjectReq(projectID, res, $rootScope.sessionToken);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+	
+						$rootScope.getAllRes = response.data;
+	
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+	
+				}
+			});
+		}
+	
+		$scope.deleteProject = function(projectID, projectTitle){
 
-        	   					$rootScope.getAllResponse = response.data;
+			var deletePopup = $ionicPopup.confirm({
+				
+				title: 'Delete ' + projectTitle,
+				template: 'Are you sure you want to delete the project?',
+			}).
+			
+			then(function(res) {
+				
+				if(res) {
+					mainFactory.deleteProjectReq(projectID, $rootScope.sessionToken);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
 
-                        }).catch(function(response){
-                            // request was not successful
-                            // handle the error
-                        });
-        	   			
-        		   }
-        	 });
-        }
-    })
-     
-    .controller('registrationController', function($scope){
+						$rootScope.getAllRes = response.data;
 
-    })
-    
-    .controller('dashboardController', function($scope) {
-        
-    })
-
-    .controller('projecteditorController', function($scope){
-        
-    })
-
-    .controller('nuBeController', function ($scope, $ionicModal) {
-
-         $ionicModal.fromTemplateUrl('templates/nutzungsbedingungen.html', {
-             scope: $scope,
-             animation: 'side-in-up',
-         }).then(function (modal) {
-             $scope.modal = modal;
-         });
-    
-         $scope.openModal = function () {
-             $scope.modal.show();
-         };
-    
-         $scope.closeModal = function () {
-             $scope.modal.hide();
-         };
-    
-         $scope.$on('$destroy', function () {
-             $scope.modal.remove();
-         })
-    })
-
-    
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+				}
+			});
+		}
+		
+		$scope.addProject = function(){
+			
+			var addInput = $ionicPopup.prompt({
+				
+				title: 'Add Project',
+				template: 'Enter new project name',
+				inputType: 'text',
+	
+			}).then(function(res) {
+	
+				if(res) {
+					mainFactory.addProjectReq(res,  $rootScope.sessionToken);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+						
+						$rootScope.getAllRes = response.data;
+						
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+				}
+			});
+		}
+	})
+	
+	.controller('registrationController', function($scope){
+	
+	})
+	
+	.controller('dashboardController', function($scope) {
+	
+	})
+	
+	.controller('projecteditorController', function($scope){
+	
+	})
+	
+	.controller('nuBeController', function ($scope, $ionicModal) {
+	
+		$ionicModal.fromTemplateUrl('templates/nutzungsbedingungen.html', {
+			scope: $scope,
+			animation: 'side-in-up',
+		}).then(function (modal) {
+			$scope.modal = modal;
+		});
+	
+		$scope.openModal = function () {
+			$scope.modal.show();
+		};
+	
+		$scope.closeModal = function () {
+			$scope.modal.hide();
+		};
+	
+		$scope.$on('$destroy', function () {
+			$scope.modal.remove();
+		})
+	})
