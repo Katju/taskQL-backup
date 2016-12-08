1374d22ed56d0317fd9c8029cb7bfa6fc6a848c6
Gregor <gj@xcdr.org>
2016-12-08 13:31:04 +0100

    generic REST request
    parametrisierte Funktion
    genericReq(sessionToken, method, url, request)
    
    …bei POST und PUT ist request = JSON, z.B.
    <example>
    var request = JSON.stringify({
    projectid : projectID,
    renameprojecttitle : res
    });
    mainFactory.genericReq($rootScope.sessionToken, "PUT",
    'https://alpha.taskql.com/rest/api/1/project/rename', request);
    </example>
    …bei GET und DELETE ist request = projectID, z.B.
    <example>
    mainFactory.genericReq($rootScope.sessionToken, "GET",
    'https://alpha.taskql.com/rest/api/1/project/getInfoById/',projectID)
    </example>

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,74 +1,155 @@
 angular
-    .module('taskQL')
-
-    .controller('registrationController', function($scope){
-
-    })
-
-    .controller('loginController', function(Login, $scope, $rootScope, $location) {
-    	
-    	$scope.login = function(){ 
-    		Login.getLogin($scope.username, $scope.password).then(function(response){
-    			$rootScope.loginResponse = response.data;
-    		    $location.path('dashboard');
-	        }).catch(function(response){
-	            //request was not successful
-	            //handle the error
-	        });	
-    	}	
-    })
-     
-    .controller('dashboardController', function(Login, $scope, $location) {
-    	$scope.getRestGetTest = function() {
-    		debugger;
-    		$location.path('gregorTest'); }
-    })
-
-    .controller('projecteditorController', function($scope){
-
-        
-    })
-    
-    .controller('restGetTestController', function(RestGet, $scope, $rootScope, $location) {
-
-    	$scope.restGetTest = function () {
-			RestGet.getRestGet($scope.restGetUrl).then(function(response) {
-				$rootScope.restGet.response = response.data;
-				$rootScope.restGet.url = $scope.restGetUrl;
-				$location.path('gregorTestResponse');
-    			    		//}
-			}).catch(function(response) {
-				debugger;
-				console.log('response is', JSON.stringify(response));
-	//		})
+	.module('taskQL')
+	.controller('mainController', function(mainFactory, $scope, $rootScope, $location, $timeout, $ionicPopup) {
+	
+		$scope.login = function(){ 
+	
+			mainFactory.getLoginReq($scope.username, $scope.password).then(function(response){
+	
+				$rootScope.loginResponse = response.data;
+				$rootScope.sessionToken = response.data.SessionToken;
+				$location.path('dashboard');
+	
+				mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+	
+					$rootScope.getAllRes = response.data;
+	
+				}).catch(function(response){
+					// request was not successful
+					// handle the error
+				});
+	
+			}).catch(function(response){
+				// request was not successful
+				// handle the error
+			});
 		}
-	)}
-    
-    })
+	
+		$scope.getSubprojects = function(projectID){
+	
+			request = projectID;
+			mainFactory.genericReq($rootScope.sessionToken, "GET", 'https://alpha.taskql.com/rest/api/1/project/getInfoById/',projectID).then(function(response){
+	
+				$rootScope.getProjectInfoRes = response.data;
+				$location.path('dashboard_subproject');
+	
+			}).catch(function(response){
+				// request was not successful
+				// handle the error
+			}); 
+		}
+	
+		$scope.renameProject = function(projectID, projectTitle){
+	
+			var renameInput = $ionicPopup.prompt({
+	
+				title: 'Rename Project',
+				template: 'Enter new name',
+				inputType: 'text',
+				inputPlaceholder: projectTitle
+	
+			}).then(function(res) {
+	
+				if(res) {
+					var request = JSON.stringify({
+						projectid : projectID,
+						renameprojecttitle : res
+					});
+					mainFactory.genericReq($rootScope.sessionToken, "PUT", 'https://alpha.taskql.com/rest/api/1/project/rename', request);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+	
+						$rootScope.getAllRes = response.data;
+	
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+	
+				}
+			});
+		}
+	
+		$scope.deleteProject = function(projectID, projectTitle){
 
-    .controller('nuBeController', function($scope, $ionicModal){
+			var deletePopup = $ionicPopup.confirm({
+				
+				title: 'Delete ' + projectTitle,
+				template: 'Are you sure you want to delete the project?',
+			}).
+			
+			then(function(res) {
+				
+				if(res) {
+					mainFactory.deleteProjectReq(projectID, $rootScope.sessionToken);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
 
-        $ionicModal.fromTemplateUrl('templates/nutzungsbedingungen.html', {
-            scope: $scope,
-            animation: 'side-in-up',
-        }).then(function(modal){
-            $scope.modal = modal;
-        });
+						$rootScope.getAllRes = response.data;
 
-        $scope.openModal = function(){
-            $scope.modal.show();
-        };
-
-        $scope.closeModal = function(){
-            $scope.modal.hide();
-        };
-
-        $scope.$on('$destroy', function(){
-            $scope.modal.remove();
-        });
-    });
-
-   
-    
-
-    
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+				}
+			});
+		}
+		
+		$scope.addProject = function(){
+			
+			var addInput = $ionicPopup.prompt({
+				
+				title: 'Add Project',
+				template: 'Enter new project name',
+				inputType: 'text',
+	
+			}).then(function(res) {
+				if(res) {
+					var request = JSON.stringify({
+						addprojecttitle : res
+					});
+					mainFactory.genericReq($rootScope.sessionToken, "POST", 'https://alpha.taskql.com/rest/api/1/project/add', request);
+					mainFactory.getAllReq($rootScope.sessionToken).then(function(response){
+						
+						$rootScope.getAllRes = response.data;
+						
+					}).catch(function(response){
+						// request was not successful
+						// handle the error
+					});
+				}
+			});
+		}
+	})
+	
+	.controller('registrationController', function($scope){
+	
+	})
+	
+	.controller('dashboardController', function($scope) {
+	
+	})
+	
+	.controller('projecteditorController', function($scope){
+	
+	})
+	
+	.controller('nuBeController', function ($scope, $ionicModal) {
+	
+		$ionicModal.fromTemplateUrl('templates/nutzungsbedingungen.html', {
+			scope: $scope,
+			animation: 'side-in-up',
+		}).then(function (modal) {
+			$scope.modal = modal;
+		});
+	
+		$scope.openModal = function () {
+			$scope.modal.show();
+		};
+	
+		$scope.closeModal = function () {
+			$scope.modal.hide();
+		};
+	
+		$scope.$on('$destroy', function () {
+			$scope.modal.remove();
+		})
+	})
