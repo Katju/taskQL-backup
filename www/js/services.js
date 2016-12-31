7cfab9a6fc24185d1d610a759a335a1134afd339
Yahima <norealnumber@gmail.com>
2016-12-31 03:43:59 +0100

    generic method update + show lastmodified
    - Alle Methoden (außer login), rufen jetzt die generische Methode
    genericReq auf.
    - else if in genericReq entfernt
    - Datum und Uhrzeit der letzten Modifikation wird angezeigt

--- www/js/services.js
+++ www/js/services.js
@@ -1,61 +1,27 @@
 angular
 .module('taskQL')
-.factory(
-		'mainFactory',
-		function($http) {
-
-			return {
-
-				getLoginReq : function(usernameInput, passwordInput) {
-					var url = 'https://alpha.taskql.com/rest/api/1/taskql/login';
-					var loginData = JSON.stringify({
-						username : usernameInput,
-						password : passwordInput
-					});
-
-					return $http.post(url, loginData);
-				},
-
-				getAllReq : function(sessionToken) {
-					var url = 'https://alpha.taskql.com/rest/api/1/project/getAll';
-
-					return $http({
-						method : 'GET',
-						url : url,
-						cache : false,
-						headers : {
-							SessionToken : sessionToken
-						}
-					})
-				},
-				
-				genericReq: function(sessionToken, method, url, request) {
-					if (method == "GET" || method == "DELETE") {
-						return $http({
-							method : method,
-							url : url + request,
-							cache : false,
-							headers : {
-								SessionToken: sessionToken
-							}
-						})
+.factory('mainFactory', function($http) {
+	return {
+		
+		getLoginReq : function(loginUrl, loginData) {
+			return $http.post(loginUrl, loginData);
+		},
+		
+		genericReq: function(sessionToken, method, url, request) {
+			if (method == "GET" || method == "DELETE" || method == "PUT" || method == "POST") {
+				return $http({
+					method : method,
+					url : url,
+					data: request,
+					cache : false,
+					headers : {
+						SessionToken: sessionToken
 					}
-					
-					else if (method == "PUT" || method == "POST") {
-						return $http({
-							method : method,
-							url: url,
-							data: request,
-							cache: false,
-							headers : {
-								SessionToken : sessionToken
-							}							
-						})
-					}
-					
-					else {
-						console.log("unerlaubte HTTP-Methode")
-					}
-				}
+				})
+			}			
+			else {
+				console.log("unerlaubte HTTP-Methode")
 			}
-		})
+		}
+	}
+})
